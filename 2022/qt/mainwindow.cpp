2138329b40b37286ef7c66252a345f4f075f208b
Wanzaz <ondra.pazourek@gmail.com>
2023-08-06 19:50:56 +0200

    Add error handling

--- 2022/qt/mainwindow.cpp
+++ 2022/qt/mainwindow.cpp
@@ -39,56 +39,67 @@ void MainWindow::execute()
     int choice = ui->comboBox->currentIndex() + 1;
     int part = ui->radioButtonp1->isChecked() ? 1 : 2;
 
-    switch (choice) {
-    case 1:
-        output_text = part == 1 ? output_text.arg(AoC.day_01_1(input)) : output_text.arg(AoC.day_01_2(input));
-        // output_text = test(input);
-        ui->textEdit->setText(output_text);
 
-        break;
-    case 2:
-        output_text = part == 1 ? output_text.arg(AoC.day_02_1()) : output_text.arg(AoC.day_02_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 3:
-        output_text = part == 1 ? output_text.arg(AoC.day_03_1()) : output_text.arg(AoC.day_03_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 4:
-        output_text = part == 1 ? output_text.arg(AoC.day_04_1()) : output_text.arg(AoC.day_04_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 5:
-        output_text = part == 1 ? output_text.arg(QString::fromStdString(AoC.day_05_1())) :
-                         "My code has really weird behaviour during running\n" + output_text.arg(QString::fromStdString(AoC.day_05_2()));
-        ui->textEdit->setText(output_text);
-        break;
-    case 6:
-        output_text = part == 1 ? output_text.arg(AoC.day_06_1()) : output_text.arg(AoC.day_06_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 7:
-        output_text = part == 1 ? output_text.arg(AoC.day_07_1()) : output_text.arg(AoC.day_07_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 8:
-        output_text = part == 1 ? output_text.arg(AoC.day_08_1()) : output_text.arg(AoC.day_08_2());
-        ui->textEdit->setText(output_text);
-        break;
-    case 9:
-        ui->textEdit->setText("NOT SOLVED YET");
-        break;
-    case 10:
-        output_text = part == 1 ? output_text.arg(AoC.day_10_1()) : output_text.arg(QString::fromStdString(AoC.day_10_2()));
-        ui->textEdit->setText(output_text);
-        break;
-    case 11:
-        output_text = output_text.arg(AoC.day_11_1());
-        ui->textEdit->setText(output_text);
-        break;
-    default:
-        break;
+    try {
+        switch (choice) {
+        case 1:
+            output_text = part == 1 ? output_text.arg(AoC.day_01_1(input)) : output_text.arg(AoC.day_01_2(input));
+            ui->textEdit->setText(output_text);
+            break;
+        case 2:
+            output_text = part == 1 ? output_text.arg(AoC.day_02_1()) : output_text.arg(AoC.day_02_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 3:
+            output_text = part == 1 ? output_text.arg(AoC.day_03_1()) : output_text.arg(AoC.day_03_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 4:
+            output_text = part == 1 ? output_text.arg(AoC.day_04_1()) : output_text.arg(AoC.day_04_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 5:
+            output_text = part == 1 ? output_text.arg(QString::fromStdString(AoC.day_05_1())) :
+                             "My code has really weird behaviour during running\n" + output_text.arg(QString::fromStdString(AoC.day_05_2()));
+            ui->textEdit->setText(output_text);
+            break;
+        case 6:
+            output_text = part == 1 ? output_text.arg(AoC.day_06_1()) : output_text.arg(AoC.day_06_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 7:
+            output_text = part == 1 ? output_text.arg(AoC.day_07_1()) : output_text.arg(AoC.day_07_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 8:
+            output_text = part == 1 ? output_text.arg(AoC.day_08_1()) : output_text.arg(AoC.day_08_2());
+            ui->textEdit->setText(output_text);
+            break;
+        case 9:
+            ui->textEdit->setText("NOT SOLVED YET");
+            break;
+        case 10:
+            output_text = part == 1 ? output_text.arg(AoC.day_10_1()) : output_text.arg(QString::fromStdString(AoC.day_10_2()));
+            ui->textEdit->setText(output_text);
+            break;
+        case 11:
+            output_text = output_text.arg(AoC.day_11_1());
+            ui->textEdit->setText(output_text);
+            break;
+        default:
+            break;
+        }
+    } catch (...) {
+        ui->textEdit->setText("Invalid input! \n"
+                              "Please put valid input into the input line above.");
     }
+
+    // It over-writes the "The result is 0" statement because of an empty input line
+    if (input.isEmpty()) {
+        ui->textEdit->setText("Invalid input! \n"
+                              "Please put valid input into the input line above.");
+    }
+
 }
 
 
